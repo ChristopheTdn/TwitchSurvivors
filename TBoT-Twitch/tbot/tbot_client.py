fac8325bf458adc6ad6b0efb176ef1de12d9a410
ToF <Christophe.taudin@gmail.com>
2023-01-12 21:20:09 +0100

    Gestion Points de chaine fichier TBOT_client.py

---
+++ TBoT-Twitch/tbot/tbot_client.py
@@ -0,0 +1,57 @@
+import twitchio
+import asyncio
+from twitchio.ext import pubsub
+import json
+
+with open('./config.json', 'r') as fichier:
+    data = json.load(fichier)
+    URLMOD = data["URLMOD"]
+
+    BOT_USERNAME = data["BOT-TWITCH"]["BOT_USERNAME"]
+    BOT_TOKEN = data["BOT-TWITCH"]["BOT_TOKEN"]
+    BOT_REFRESH = data["BOT-TWITCH"]["BOT_REFRESH"]
+    BOT_VALID = data["BOT-TWITCH"]["BOT_VALID"]
+    BOT_USER_ID = data["BOT-TWITCH"]["BOT_USER_ID"]
+    BOT_ID = data["BOT-TWITCH"]["BOT_ID"]
+    BOT_NICK = data["BOT-TWITCH"]["BOT_NICK"]
+    BOT_PREFIX = data["BOT-TWITCH"]["BOT_PREFIX"]
+    BOT_CHANNEL = data["BOT-TWITCH"]["BOT_CHANNEL"]
+
+    CLIENT_USERNAME = data["BOT-TWITCH"]["CLIENT_USERNAME"]
+    CLIENT_TOKEN = data["BOT-TWITCH"]["CLIENT_TOKEN"]
+    CLIENT_REFRESH = data["BOT-TWITCH"]["CLIENT_REFRESH"]
+    CLIENT_USER_ID = data["BOT-TWITCH"]["CLIENT_USER_ID"]
+    CLIENT_VALID = data["BOT-TWITCH"]["CLIENT_VALID"]
+    CLIENT_ID = data["BOT-TWITCH"]["CLIENT_ID"]
+    CLIENT_NICK = data["BOT-TWITCH"]["CLIENT_NICK"]
+    CLIENT_PREFIX = data["BOT-TWITCH"]["CLIENT_PREFIX"]
+    CLIENT_CHANNEL = data["BOT-TWITCH"]["CLIENT_CHANNEL"]
+
+import twitchio
+from twitchio.ext import pubsub
+
+users_oauth_token = CLIENT_TOKEN
+users_channel_id = CLIENT_USER_ID
+client = twitchio.Client("7avqdwvhfq48n3511t21px7e7rjmjy",  initial_channels=["GToF_"])
+client.pubsub = pubsub.PubSubPool(client)
+
+async def event_pubsub_channel_points2(event: pubsub.PubSubChannelPointsMessage):
+    print('Redemption by ', event.user.name, 'of reward', event.reward.title, 'with text', event.reward.prompt,
+          'done')
+     
+@client.event()
+async def event_ready():
+    print(f"Ready | {client.nick}")
+    topics = [
+    pubsub.channel_points(users_oauth_token)[users_channel_id],
+    pubsub.bits(users_oauth_token)[users_channel_id],]
+    await client.pubsub.subscribe_topics(topics)
+
+@client.event()
+async def event_pubsub_channel_points(event: pubsub.PubSubChannelPointsMessage):
+    print (event.reward.title, ": Pour un cout de ", event.reward.cost)
+    channel = event.channel_id
+    
+    await event_pubsub_channel_points2(event)
+
+client.run()
